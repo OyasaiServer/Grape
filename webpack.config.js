2255ed7910e9ffe43b776558a2ede9e96a161523
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-08-09 17:55:31 -0400

    fix: exclude node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
             },
             {
                 test: /\.json$/,
-                use: 'json-loader'
+                use: 'json-loader',
+                exclude: /node_modules/
             }
         ]
     },
