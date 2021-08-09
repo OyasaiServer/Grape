c944c97ee4623ea5e803a4083ca85e9ff95c4ab9
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-08-09 18:30:22 -0400

    feat(socket): add socket

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import { ChildProcess, spawn } from 'child_process'
 import { ServerConfig } from './types'
+import { Server as Socket } from 'socket.io'
 
 export default class Server implements ServerConfig {
     private instance?: ChildProcess
     location: string
     args: string[]
+    socket?: Socket
     constructor({ location, args }: ServerConfig) {
         this.location = location
         this.args = args
+        this.socket = new Socket()
     }
-
     start() {
         if (this.instance) throw Error('Server has already started!')
         this.instance = spawn(
